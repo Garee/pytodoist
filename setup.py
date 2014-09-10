072080214a1013ddf3dae22abb0ccff3ffa921ac
Gary Blackwood <gareeblackwood@gmail.com>
2014-09-10 20:06:49 +0100

    Conforming to PEP-8!

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
+"""Setup script for the pytodoist package."""
+
 from distutils.core import setup
 
-setup(name = 'pytodoist',
-      version = '1.0',
-      license = 'MIT',
-      description = 'A python wrapper for the Todoist API.',
-      long_description = open('README').read(),
-      author = 'Gary Blackwood',
-      author_email = 'gary@garyblackwood.co.uk',
-      url = 'http://www.github.com/Garee/PyTodoist',
-      packages = ['pytodoist'],
-      install_requires = ['requests'],)
+setup(name='pytodoist',
+      version='1.0',
+      license='MIT',
+      description='A python wrapper for the Todoist API.',
+      long_description=open('README').read(),
+      author='Gary Blackwood',
+      author_email='gary@garyblackwood.co.uk',
+      url='http://www.github.com/Garee/PyTodoist',
+      packages=['pytodoist'],
+      install_requires=['requests'],)
