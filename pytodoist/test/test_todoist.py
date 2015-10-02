8360252d7dc109c56adb044b7936c8a7aab5fb11
Gary Blackwood <gareeblackwood@gmail.com>
2015-10-02 08:49:05 +0100

    No longer asserting for exception raised in test_add_location_reminder as it is no longer premium only.

--- pytodoist/test/test_todoist.py
+++ pytodoist/test/test_todoist.py
@@ -363,10 +363,9 @@ class TaskTest(unittest.TestCase):
         self.task.add_date_reminder('email', '2050-3-24T23:59')
 
     def test_add_location_reminder(self):
-        with self.assertRaises(todoist.RequestError):  # Premium only.
-            self.task.add_location_reminder('email', 'Leave Glasgow',
-                                            55.8580, 4.2590, 'on_leave',
-                                            100)
+        self.task.add_location_reminder('email', 'Leave Glasgow',
+                                        55.8580, 4.2590, 'on_leave',
+                                        100)
 
 
 if __name__ == '__main__':
