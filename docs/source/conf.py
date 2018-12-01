397e16ca59a97cfef87858375330d9924e0140bc
Gary Blackwood <gary@garyblackwood.co.uk>
2018-12-01 20:01:00 +0000

    Removed readthedocs webhook from travis configuration.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyTodoist'
-copyright = u'2014-2016, Gary Blackwood'
+copyright = u'2014-2018, Gary Blackwood'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
